75979d677514997c702338bd9624331add693d95
JCC <49532511+JiangChongCong@users.noreply.github.com>
2020-11-22 17:18:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 Dataset_Path = dict(
-    CULane = "/workspace/CULANE_DATASET",
-    Tusimple = "/workspace/TUSIMPLE_DATASET",
-    bdd100k = "/workspace/BDD100K_DATASET"
+    CULane = "/content/drive/MyDrive/CULANE_DATASET"
 )
